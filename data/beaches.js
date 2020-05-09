3d6dcad49b1952791b24b2137482a7586ddc3790
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-05-09 12:16:13 +0200

    adding data for beaches

---
+++ data/beaches.js
@@ -0,0 +1,52 @@
+const beaches = [
+  {
+    name: "Wannsee",
+    imageUrl: "https://res.cloudinary.com/dqlvehu5z/image/upload/v1589017957/Berlin%20Beach/Wannsee_su1saw.jpg",
+    price: 5.50,
+    priceCurrency: "EUR",
+    lat: 48.884211,
+    lng: 2.34689
+  },
+  {
+    name: "Weißensee",
+    imageUrl: "https://res.cloudinary.com/dqlvehu5z/image/upload/v1589017961/Berlin%20Beach/wei%C3%9Fensee_zlkzau.jpg",
+    price: 5.50,
+    priceCurrency: "EUR",
+    lat: 52.5539,
+    lng: 13.4675
+  },
+  {
+    name: "Nymphensee",
+    imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg",
+    price: 3.50,
+    priceCurrency: "EUR",
+    lat: 52.5620,
+    lng: 13.0770
+  },
+  {
+    name: "Jungfernheide",
+    imageUrl: "https://res.cloudinary.com/dqlvehu5z/image/upload/v1589017962/Berlin%20Beach/jungfernheide_udkpk5.jpg",
+    price: 6,
+    priceCurrency: "EUR",
+    lat: 52.5307,
+    lng: 13.3005
+  },
+  {
+    name: "Müggelsee",
+    imageUrl: "https://res.cloudinary.com/dqlvehu5z/image/upload/v1589019146/Berlin%20Beach/mu%CC%88ggelsee_sz6bny.jpg",
+    price: 0,
+    priceCurrency: "EUR",
+    lat: 52.4349,
+    lng: 13.6351
+  },
+  {
+    name: "Tegeler See",
+    imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat6.jpg",
+    price: 0,
+    priceCurrency: "EUR",
+    lat: 52.5749,
+    lng: 13.2572
+  }
+];
+
+export default beaches;
