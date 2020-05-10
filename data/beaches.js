0658763e6e40ff201a4ec5174fc0e4c64a575362
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-05-10 10:06:47 +0200

    adding style to beach cardd

--- data/beaches.js
+++ data/beaches.js
@@ -3,7 +3,7 @@ const beaches = [
     name: "Wannsee",
     imageUrl: "https://res.cloudinary.com/dqlvehu5z/image/upload/v1589017957/Berlin%20Beach/Wannsee_su1saw.jpg",
     price: 5.50,
-    priceCurrency: "EUR",
+    priceCurrency: "EUR per day",
     lat: 48.884211,
     lng: 2.34689
   },
@@ -11,15 +11,15 @@ const beaches = [
     name: "Weißensee",
     imageUrl: "https://res.cloudinary.com/dqlvehu5z/image/upload/v1589017961/Berlin%20Beach/wei%C3%9Fensee_zlkzau.jpg",
     price: 5.50,
-    priceCurrency: "EUR",
+    priceCurrency: "EUR per day",
     lat: 52.5539,
     lng: 13.4675
   },
   {
     name: "Nymphensee",
-    imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg",
+    imageUrl: "https://res.cloudinary.com/dqlvehu5z/image/upload/v1589017957/Berlin%20Beach/brieselang_itcntq.jpg",
     price: 3.50,
-    priceCurrency: "EUR",
+    priceCurrency: "EUR per day",
     lat: 52.5620,
     lng: 13.0770
   },
@@ -27,7 +27,7 @@ const beaches = [
     name: "Jungfernheide",
     imageUrl: "https://res.cloudinary.com/dqlvehu5z/image/upload/v1589017962/Berlin%20Beach/jungfernheide_udkpk5.jpg",
     price: 6,
-    priceCurrency: "EUR",
+    priceCurrency: "EUR per day",
     lat: 52.5307,
     lng: 13.3005
   },
@@ -35,15 +35,15 @@ const beaches = [
     name: "Müggelsee",
     imageUrl: "https://res.cloudinary.com/dqlvehu5z/image/upload/v1589019146/Berlin%20Beach/mu%CC%88ggelsee_sz6bny.jpg",
     price: 0,
-    priceCurrency: "EUR",
+    priceCurrency: "EUR per day",
     lat: 52.4349,
     lng: 13.6351
   },
   {
     name: "Tegeler See",
-    imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat6.jpg",
+    imageUrl: "https://res.cloudinary.com/dqlvehu5z/image/upload/v1589019145/Berlin%20Beach/tegel_yjesgw.jpg  ",
     price: 0,
-    priceCurrency: "EUR",
+    priceCurrency: "EUR per day",
     lat: 52.5749,
     lng: 13.2572
   }
